7d246fdd569f3c14050247508d81c0e084916b3a
unknown <L00187850@atu.ie>
2024-10-14 22:11:34 +0100

    this is a test

---
+++ sample.bat
@@ -0,0 +1,26 @@
+:: By: Philip Stewart
+:: Date: 14 Oct 2024
+:: Function: Performing a commit
+
+:: This is a sample bat file to prove that a commit can be performed via bat file
+
+cls
+git status
+
+echo **************************************************
+echo Performing an add for all files in this directory
+git add .
+git status
+
+echo **************************************************
+set /p CommitMessage=Enter the commit message: 
+git commit -m "%CommitMessage%"
+git status
+
+echo **************************************************
+echo Pushing to GITHUB repository
+git push -u origin master
+echo **************************************************
+
+echo Done!
+pause
